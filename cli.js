721634d9471b2c79ca588bf938829c72cf59d649
Gluons <sarunta@gmail.com>
2016-07-26 23:34:19 +0700

    Switch from "commander" to "yargs".

--- cli.js
+++ cli.js
@@ -1,32 +1,32 @@
 'use strict';
-const program = require('commander');
 
-const packageInfo = require('./package.json');
+const description = 'Clone your default configurations to current working directory.';
+
+const argv = require('yargs')
+			.version()
+			.alias('version', 'V')
+			.usage(`Usage: confc [options] [filename..]\n\n${description}`)
+			.string('p')
+			.alias('p', 'path')
+			.describe('p', 'Path to default configuration files.')
+			.boolean('v')
+			.alias('v', 'verbose')
+			.describe('v', 'Display more information.')
+			.help('h')
+			.alias('h', 'help')
+			.example('comfc --path ~ .editorconfig')
+			.argv;
 
 const confc = require('./');
 
-var files;
+var files = argv._;
 
-program
-	.version(packageInfo.version)
-	.usage(`[options] [filename ...]`)
-	.description(`Clone your default configurations to current working directory.`)
-	.arguments(`[fileNames...]`)
-	.action((fileNames) => {
-		if (typeof fileNames !== 'undefined') {
-			files = fileNames;
-		}
-	})
-	.option(`-p, --path [path]`, `Path to default configuration files.`, (p) => p.replace(/"|'/g, ''))
-	.option(`-v, --verbose`, `Display more information.`)
-	.parse(process.argv);
+if ((typeof argv.path === 'string') && (argv.path.length > 0)) {
+	confc.config.path = argv.path;
+}
 
-if (typeof program.path === 'string') {
-	confc.config.path = program.path;
-}
-if (program.verbose) {
-	confc.config.verbose = true;
-}
+confc.config.verbose = argv.verbose;
+
 if (Array.isArray(files) && (files.length > 0)) {
 	confc.copy(files).catch((errors) => {
 		confc.utils.displayErrors(errors);
