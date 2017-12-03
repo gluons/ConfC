4e916e80ea56e55a548ffac36fc3f23186149db1
Saran Tanpituckpong <sarunta@gmail.com>
2017-12-03 20:14:49 +0700

    :tada: Use Docute 3

---
+++ README.md
@@ -0,0 +1,7 @@
+# ConfC
+
+GitHub pages documentation.
+
+Go to https://gluons.github.io/ConfC/
+
+Made with [📜 Docute](https://github.com/egoist/docute).
---
+++ config.js
@@ -0,0 +1,15 @@
+docute.init({
+	url: 'https://raw.githubusercontent.com/gluons/ConfC/master/',
+	nav: [
+		{ title: 'Home', path: '/' }
+	],
+	repo: 'gluons/ConfC',
+	icons: [
+		{
+			label: 'Follow me on Google+',
+			svgId: 'google-plus',
+			svgClass: 'google-plus-icon',
+			link: 'https://www.google.com/+SaranTanpituckpong'
+		}
+	]
+});
